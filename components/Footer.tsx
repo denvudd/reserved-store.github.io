34d122404ec1a28a0a2e9e75c56fb3ae13fbde79
Dima <dimontv7@gmail.com>
2023-08-28 17:29:00 +0300

    create Container & MainNav components, create types dir

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 
-interface FooterProps {
-  
-}
+const Footer: React.FC = ({}) => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 Reserved, Inc. All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-const Footer: React.FC<FooterProps> = ({}) => {
-return <footer className="bg-white border-t">
-  <div className="mx-auto py-10">
-    <p className="text-center text-xs text-black">
-      &copy; 2023 Reserved, Inc. All rights reserved
-    </p>
-  </div>
-</footer>
-}
-
-export default Footer
+export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Container from "./ui/Container";
+import Link from "next/link";
+import MainNav from "./MainNav";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
-  return <div className="border-b">Navbar</div>;
+  return (
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href={`/`} className="mr-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl uppercase">Store</p>
+          </Link>
+          <MainNav data={[]}/>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Navbar;
