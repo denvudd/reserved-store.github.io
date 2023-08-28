cc927f9a379a6dd6cd2fc7d7620cc043b22406a0
Dima <dimontv7@gmail.com>
2023-08-28 19:36:52 +0300

    create billboard fetch and display billboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+import { getBillboard } from "@/actions/get-billboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
 
-export default function Home() {
-  return <div className="">Hello store</div>;
-}
+export const revalidate = 0;
+const Home = async () => {
+  const billboard = await getBillboard("db7b4abf-3ca4-461b-8563-11e9e3f89eca");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard billboard={billboard} labelRequired={false} />
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
